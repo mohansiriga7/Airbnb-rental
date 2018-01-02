76140ea8b7506a2ac978b758545cb1411e67b100
mohansiriga7 <ms7.developer@gmail.com>
2018-01-02 07:09:40 +0530

    Specified the attribute that we want Geocoder to convert, in this case the address.

--- app/models/airbnb_host_place.rb
+++ app/models/airbnb_host_place.rb
@@ -1,2 +1,8 @@
 class AirbnbHostPlace < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :price, presence: true
 end
